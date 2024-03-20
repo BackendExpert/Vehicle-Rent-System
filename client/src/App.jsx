42ae2fb86113ec07a1cd0d971a6fcf98737143ab
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-20 21:09:14 +0530

    Update App.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+vinurapathirana@users.noreply.github.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./comportments/LoginSignUp/Login";
 import SignUp from "./comportments/LoginSignUp/SignUp";
+import Dashborad from "./comportments/Dashboard/Dashborad";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/Register" element={<SignUp />} />        
+        <Route path="/Register" element={<SignUp />} />   
+
+        <Route path="/Dashboard" element={<Dashborad />} />     
       </Routes>
     </BrowserRouter>
   )
