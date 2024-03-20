104b61e7b2a088a65a240139ffbe6f90cda0e7d6
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-20 22:21:41 +0530

    Update Dashborad.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+vinurapathirana@users.noreply.github.com>

--- client/src/comportments/Dashboard/Dashborad.jsx
+++ client/src/comportments/Dashboard/Dashborad.jsx
@@ -3,8 +3,32 @@ import { useState } from "react"
 import { Link } from "react-router-dom"
 
 const Dashborad = () => {
+    //for open and close Side bar
+    const [sideOpen, SetsideOpen] = useState();
+
+    //curent login user
+    const RoleUser = secureLocalStorage.getItem("Login1");
+
+    //SuperAdmin Side list
+    const SuperAdminSide = [
+        {name: "Users", link: "#", icon: <Icons name="person" size="large"></Icons>},
+        {name: "Users", link: "#", icon: <Icons name="person" size="large"></Icons>},
+        {name: "Users", link: "#", icon: <Icons name="person" size="large"></Icons>},
+        {name: "Users", link: "#", icon: <Icons name="person" size="large"></Icons>},
+    ]
+
   return (
-    <div>Dashborad</div>
+    <div>
+        {
+            SuperAdminSide.map((superSide) => {
+                return (
+                    <div className="">
+                        {superSide.name}
+                    </div>
+                )
+            })
+        }
+    </div>
   )
 }
 
