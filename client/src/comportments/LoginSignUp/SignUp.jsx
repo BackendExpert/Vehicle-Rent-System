c2f7976b2d7108cca2102d533fc7c7811ce1de90
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-20 19:49:19 +0530

    Update SignUp.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+vinurapathirana@users.noreply.github.com>

--- client/src/comportments/LoginSignUp/SignUp.jsx
+++ client/src/comportments/LoginSignUp/SignUp.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const SignUp = () => {
   return (
     <div>
-        
+        <div className="bg-[url(https://wallpapercave.com/wp/qkz7ffi.jpg)] bg-center bg-cover h-screen w-full'">
+
+        </div>
     </div>
   )
 }
