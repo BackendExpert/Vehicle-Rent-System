63fa1e88ab015f9bc351a135186a5a68c192f447
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-20 19:28:59 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+vinurapathirana@users.noreply.github.com>

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const mysql = require('mysql2');
+const bcrypt = require('bcryptjs');
+const multer = require('multer');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+
+const path = require('path')
+
+const resourceLimits = require('worker_threads');
