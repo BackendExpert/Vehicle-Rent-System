f914527979c470f46f66fb69b16b276a2c4593ad
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-20 19:46:18 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+vinurapathirana@users.noreply.github.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,11 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 export default function App() {
   return (
-    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={} />
+      </Routes>
+    </BrowserRouter>
   )
 }
