6d1276d056b2d12de51eb04b5681ec4927345d11
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-20 20:06:19 +0530

    Update SignUp.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+vinurapathirana@users.noreply.github.com>

--- client/src/comportments/LoginSignUp/SignUp.jsx
+++ client/src/comportments/LoginSignUp/SignUp.jsx
@@ -15,6 +15,8 @@ const SignUp = () => {
     // make onChange 
     const onChange = (e) => 
     SetDataForm({...dataForm, [e.target.name]: e.target.value })
+
+
   return (
     <div className='bg-[url(https://wallpapercave.com/wp/wp2707503.jpg)] bg-center bg-cover h-screen w-full'>
         <div className="lg:py-[150px] lg:mx-40 py-16 mx-8">
@@ -25,15 +27,18 @@ const SignUp = () => {
                     <form>
                         <div className="my-4">
                             <label htmlFor="" className='text-xl'>Enter Name : </label>
-                            <input type="text" className="my-2 w-full h-11 border rounded pl-2" name='username' required placeholder='Enter Name' />
+                            <input type="text" className="my-2 w-full h-11 border rounded pl-2" name='username' required placeholder='Enter Name' 
+                            value={username} onChange={onChange}/>
                         </div>
                         <div className="my-4">
                             <label htmlFor="" className='text-xl'>Enter Email : </label>
-                            <input type="email" className="my-2 w-full h-11 border rounded pl-2" name='email' required placeholder='Enter Email Address' />
+                            <input type="email" className="my-2 w-full h-11 border rounded pl-2" name='email' required placeholder='Enter Email Address'  
+                            value={email} onChange={onChange}/>
                         </div>
                         <div className="my-4">
                             <label htmlFor="" className='text-xl'>Enter Password : </label>
-                            <input type="password" className="my-2 w-full h-11 border rounded pl-2" name='password' required placeholder='Enter Password'/>
+                            <input type="password" className="my-2 w-full h-11 border rounded pl-2" name='password' required placeholder='Enter Password' 
+                            value={password} onChange={onChange}/>
                         </div>
                         <div className="my-4">
                             <button type="submit" className="w-1/2 h-12 border border-blue-500 rounded text-blue-500 duration-500 hover:text-white hover:bg-blue-500">Register</button>
