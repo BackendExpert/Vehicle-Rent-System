ab19680079f9b912ab341b2f5cc19d17baaba172
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-20 19:53:29 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+vinurapathirana@users.noreply.github.com>

--- client/src/comportments/LoginSignUp/SignUp.jsx
+++ client/src/comportments/LoginSignUp/SignUp.jsx
@@ -3,35 +3,37 @@ import { Link } from 'react-router-dom'
 
 const SignUp = () => {
   return (
-    <div>
     <div className='bg-[url(https://wallpapercave.com/wp/qkz7ffi.jpg)] bg-center bg-cover h-screen w-full'>
-        <div className="lg:py-24 lg:mx-40 py-16 mx-8">
+        <div className="lg:py-[150px] lg:mx-40 py-16 mx-8">
             <div className="bg-white lg:py-8 lg:px-24 py-10 px-8 rounded-md">
-                <h1 className="text-3xl font-semibold">Login Here</h1>
+                <h1 className="text-3xl font-semibold">Register Here</h1>
                 
                 <div className="pl-4">
-                    <form onSubmit={headleLogin}>
+                    <form>
+                        <div className="my-4">
+                            <label htmlFor="" className='text-xl'>Enter Name : </label>
+                            <input type="text" className="my-2 w-full h-11 border rounded pl-2" name='username' required placeholder='Enter Name' />
+                        </div>
                         <div className="my-4">
                             <label htmlFor="" className='text-xl'>Enter Email : </label>
-                            <input type="email" className="my-2 w-full h-11 border rounded pl-2" required placeholder='Enter Email Address' name='email' onChange={onChange} value={email}/>
+                            <input type="email" className="my-2 w-full h-11 border rounded pl-2" name='email' required placeholder='Enter Email Address' />
                         </div>
                         <div className="my-4">
                             <label htmlFor="" className='text-xl'>Enter Password : </label>
-                            <input type="password" className="my-2 w-full h-11 border rounded pl-2" required placeholder='Enter Password' name='password' onChange={onChange} value={password}/>
+                            <input type="password" className="my-2 w-full h-11 border rounded pl-2" name='password' required placeholder='Enter Password'/>
                         </div>
                         <div className="my-4">
-                            <button type="submit" className="w-1/2 h-12 border border-blue-500 rounded text-blue-500 duration-500 hover:text-white hover:bg-blue-500">Login</button>
+                            <button type="submit" className="w-1/2 h-12 border border-blue-500 rounded text-blue-500 duration-500 hover:text-white hover:bg-blue-500">Register</button>
                         </div>
                     </form>
-                    <p>Dont't have an Account ? 
-                        <Link to={'/Register'}>
-                            <span className="pl-2 text-blue-500">Create New</span>
+                    <p>Already have an Account ?
+                        <Link to={'/'}>
+                            <span className="pl-2 text-blue-500">SignIn</span>
                         </Link>
                     </p>
                 </div>
             </div>
         </div>
-    </div>
     </div>
   )
 }
