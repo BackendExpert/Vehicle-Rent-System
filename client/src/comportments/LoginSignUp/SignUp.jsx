1814746e82f2e0d60d8fafadfb4f027372b89605
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-20 19:49:52 +0530

    Update SignUp.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+vinurapathirana@users.noreply.github.com>

--- client/src/comportments/LoginSignUp/SignUp.jsx
+++ client/src/comportments/LoginSignUp/SignUp.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const SignUp = () => {
   return (
     <div>
-        <div className="bg-[url(https://wallpapercave.com/wp/qkz7ffi.jpg)] bg-center bg-cover h-screen w-full'">
-
+    <div className='bg-[url(https://wallpapercave.com/wp/qkz7ffi.jpg)] bg-center bg-cover h-screen w-full'>
+        <div className="lg:py-24 lg:mx-40 py-16 mx-8">
+            <div className="bg-white lg:py-8 lg:px-24 py-10 px-8 rounded-md">
+                <h1 className="text-3xl font-semibold">Login Here</h1>
+                
+                <div className="pl-4">
+                    <form onSubmit={headleLogin}>
+                        <div className="my-4">
+                            <label htmlFor="" className='text-xl'>Enter Email : </label>
+                            <input type="email" className="my-2 w-full h-11 border rounded pl-2" required placeholder='Enter Email Address' name='email' onChange={onChange} value={email}/>
+                        </div>
+                        <div className="my-4">
+                            <label htmlFor="" className='text-xl'>Enter Password : </label>
+                            <input type="password" className="my-2 w-full h-11 border rounded pl-2" required placeholder='Enter Password' name='password' onChange={onChange} value={password}/>
+                        </div>
+                        <div className="my-4">
+                            <button type="submit" className="w-1/2 h-12 border border-blue-500 rounded text-blue-500 duration-500 hover:text-white hover:bg-blue-500">Login</button>
+                        </div>
+                    </form>
+                    <p>Dont't have an Account ? 
+                        <Link to={'/Register'}>
+                            <span className="pl-2 text-blue-500">Create New</span>
+                        </Link>
+                    </p>
+                </div>
+            </div>
         </div>
+    </div>
     </div>
   )
 }
