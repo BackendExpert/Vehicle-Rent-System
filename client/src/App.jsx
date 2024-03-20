3d50ea7c829845e28598891d7bed9d3fdf4fb951
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-20 19:47:28 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+vinurapathirana@users.noreply.github.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./comportments/LoginSignUp/Login";
+import SignUp from "./comportments/LoginSignUp/SignUp";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={} />
+        <Route path="/" element={<Login />} />
+        <Route path="/Register" element={<SignUp />} />        
       </Routes>
     </BrowserRouter>
   )
--- client/src/comportments/LoginSignUp/SignUp.jsx
+++ client/src/comportments/LoginSignUp/SignUp.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div>
+        
+    </div>
   )
 }
 
