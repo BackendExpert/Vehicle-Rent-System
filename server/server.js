938dcb857147c5d6f2aa124188e5eba42abc7c13
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-20 19:40:56 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+vinurapathirana@users.noreply.github.com>

--- server/server.js
+++ server/server.js
@@ -8,3 +8,17 @@ const cors = require('cors');
 const path = require('path')
 
 const resourceLimits = require('worker_threads');
+
+const app = express();
+const PORT = process.env.PORT || 8081
+
+
+// middleware
+app.use(express.json())
+app.use(cors())
+app.use(express.static('public')); 
+
+
+
+//check the server is working
+app.listen(PORT, () => console.log(`Server is Running on PORT ${PORT}`));
