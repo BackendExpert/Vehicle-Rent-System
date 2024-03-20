dd700e73918690a06b963969d8818198d6a5a4a2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-20 22:32:35 +0530

    Update Dashborad.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+vinurapathirana@users.noreply.github.com>

--- client/src/comportments/Dashboard/Dashborad.jsx
+++ client/src/comportments/Dashboard/Dashborad.jsx
@@ -6,6 +6,7 @@ import  secureLocalStorage  from  "react-secure-storage"
 const Dashborad = (children) => {
     //for open and close Side bar
     const [sideOpen, SetsideOpen] = useState();
+    const [navOpen, SetNavOpen] = useState();
 
     //curent login user
     const RoleUser = secureLocalStorage.getItem("Login1");
@@ -70,7 +71,31 @@ const Dashborad = (children) => {
                 </div>
                 {/* side menu end */}
                 {/* nav bar start */}
-                    
+                <div className="flex justify-between border-b-4 border-blue-300 shadow-xl mt-4 bg-white py-4 px-6 rounded">
+                    NIFS
+                    <div className="text-xl absolute cursor-pointer lg:hidden right-8" onClick={() => SetNavOpen(!navOpen)}>
+                        <Icons name={navOpen ? 'close' : 'menu'} ></Icons>
+                    </div>
+                    <div className={`rounded lg:border-0 border-b-4 border-blue-400 lg:flex lg:items-center absolute lg:static bg-white transition-all lg:mt-0 mt-12 ${navOpen ? 'lg:visible':'lg:visible invisible'}`}>
+
+                        {
+                            navLists.map((nav) => {
+                                if(nav.desc === "logout"){
+                                    return (
+                                        <div onClick={logout} className="cursor-pointer text-red-500 px-4 lg:py-0 py-4">{nav.name}</div>
+                                    )
+                                }
+                                else{
+                                    return (
+                                        <Link to={nav.link}>
+                                            <div className="cursor-pointer text-blue-500 px-4 lg:py-0 py-4 lg:border-0 border-b-4 border-blue-200">{nav.name}</div>
+                                        </Link>                            
+                                    )
+                                }
+                            })
+                        }
+                    </div>
+                </div>
                 {/* nav bar end */}
             </div>
         </div>
