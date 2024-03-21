98009c710454465370b8d5e227ade20d9066cdd0
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-21 12:14:57 +0530

    Update Dashborad.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+VinuraPathirana@users.noreply.github.com>
    Co-Authored-By: R.M.V.A.B Rajakaruna <160888999+infovish001@users.noreply.github.com>

--- client/src/comportments/Dashboard/Dashborad.jsx
+++ client/src/comportments/Dashboard/Dashborad.jsx
@@ -25,6 +25,7 @@ const Dashborad = (children) => {
     //SuperAdmin Side list
     const SuperAdminSide = [
         {name: "Users", link: "#", icon: <Icons name="people" size="large"></Icons>},
+        {name: "Requests", link: "#", icon: <Icons name="help-circle" size="large"></Icons>},
         {name: "Vehicles", link: "#", icon: <Icons name="car" size="large"></Icons>},
         {name: "Report", link: "#", icon: <Icons name="document-text" size="large"></Icons>},
         {name: "Suspended", link: "#", icon: <Icons name="person-circle" size="large"></Icons>},
