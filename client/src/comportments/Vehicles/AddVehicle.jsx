aae3774c50847f41a0ff98bd3d3c884663bfe986
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-21 15:05:17 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+VinuraPathirana@users.noreply.github.com>
    Co-Authored-By: R.M.V.A.B Rajakaruna <160888999+infovish001@users.noreply.github.com>

--- client/src/comportments/Vehicles/AddVehicle.jsx
+++ client/src/comportments/Vehicles/AddVehicle.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import Icons from "@reacticons/ionicons"
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import  secureLocalStorage  from  "react-secure-storage"
 
 const AddVehicle = () => {
-  return (
-    <div>AddVehicle</div>
-  )
+    const navigate = useNavigate();
+
+    //curent login user
+    const RoleUser = secureLocalStorage.getItem("Login1");
+
+    if(RoleUser === "SuperAdmin" || RoleUser === "Admin" || RoleUser === "renter"){
+        return (
+            <div>AddVehicle</div>
+        )
+    }
+    else{
+        useEffect(() => {
+            navigate('/unauthorizedAccess');
+        }, [])
+    }
+
 }
 
 export default AddVehicle
