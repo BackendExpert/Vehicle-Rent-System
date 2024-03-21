da9ecc3f984205dcf9fc0ac632a3c2e21f8b7d33
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-21 11:22:32 +0530

    Update SignUp.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+VinuraPathirana@users.noreply.github.com>
    Co-Authored-By: R.M.V.A.B Rajakaruna <160888999+infovish001@users.noreply.github.com>

--- client/src/comportments/LoginSignUp/SignUp.jsx
+++ client/src/comportments/LoginSignUp/SignUp.jsx
@@ -29,7 +29,7 @@ const SignUp = () => {
 
   return (
     <div className='bg-[url(https://wallpapercave.com/wp/wp2707503.jpg)] bg-center bg-cover h-screen w-full'>
-        <div className="lg:py-[150px] lg:mx-40 py-16 mx-8">
+        <div className="lg:py-[50px] lg:mx-40 py-16 mx-8">
             <div className="bg-white lg:py-8 lg:px-24 py-10 px-8 rounded-md">
                 <h1 className="text-3xl font-semibold">Register Here</h1>
                 
