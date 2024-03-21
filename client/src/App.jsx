31d005c785d6540a7b20c2c02a8af8deda417f0c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-21 12:33:36 +0530

    Update App.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+VinuraPathirana@users.noreply.github.com>
    Co-Authored-By: R.M.V.A.B Rajakaruna <160888999+infovish001@users.noreply.github.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import Login from "./comportments/LoginSignUp/Login";
 import SignUp from "./comportments/LoginSignUp/SignUp";
 import Dashborad from "./comportments/Dashboard/Dashborad";
 import PrivateRoute from "./comportments/PrivateRoute";
+import Vehicles from "./comportments/Vehicles/Vehicles";
+import AddVehicle from "./comportments/Vehicles/AddVehicle";
 
 export default function App() {
   return (
@@ -11,7 +13,18 @@ export default function App() {
         <Route path="/" element={<Login />} />
         <Route path="/Register" element={<SignUp />} />   
 
+        {/* Following routes are protected by PrivateRoute */}
+
+        {/* 
+          when all login users redirect to the Dashboard 
+          and routes of dashbord will assign according to login user role
+        */}
         <Route path="/Dashboard" element={<PrivateRoute><Dashborad /></PrivateRoute>} /> 
+
+        {/* This is Vehicle route this can access by all users */}
+        <Route path="/Vehicles" element={<PrivateRoute><Vehicles /></PrivateRoute> } />
+        <Route path="/AddVehicles" element={<PrivateRoute><AddVehicle /></PrivateRoute> } />
+         
       </Routes>
     </BrowserRouter>
   )
