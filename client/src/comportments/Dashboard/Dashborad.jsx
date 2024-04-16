d7c3410e16e35affe98e3a714ccf1b65a0bae52a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-16 11:35:38 +0530

    Update Dashborad.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+VinuraPathirana@users.noreply.github.com>
    Co-Authored-By: R.M.V.A.B Rajakaruna <160888999+infovish001@users.noreply.github.com>

--- client/src/comportments/Dashboard/Dashborad.jsx
+++ client/src/comportments/Dashboard/Dashborad.jsx
@@ -51,126 +51,132 @@ const Dashborad = (children) => {
 
     // https://cdn-icons-png.flaticon.com/128/3135/3135715.png
 
-    return (
-        <div className="bg-gray-200 py-2 h-auto">
-            <div className="flex">
-                <div className={`duration-500 relative border-r-4 border-blue-300 shadow-xl my-1 mx-2 rounded bg-white h-auto pl-4 py-4 ${sideOpen ? "w-64" : "w-20" }`}>
-                    <div className="flex">
-                        <div className="text-[#3B71CA] pt-1" onClick={() => SetsideOpen(!sideOpen)}>{sideOpen ? <Icons size="large" name="close"></Icons> : <Icons size="large" name="menu"></Icons>}</div>
-                        {/* <div className={`text-2xl pb-4 text-[#3B71CA] font-bold ${!sideOpen && 'scale-0'}`}>SuperAdmin</div>                     */}
-                    </div>
-                    <div className="">
-                        {sideOpen ? <div>
-                            <img src="https://cdn-icons-png.flaticon.com/128/3135/3135715.png" className="px-6"/>
-                        </div> : <div></div>}
-                    </div>
-                    <div className={`pl-2 text-xl text-gray-400 duration-500 hover:text-[#3B71CA]`}>
-                        <Link to={'/Dashboard'}>
-                            {sideOpen ? <div className="flex pl-2 pt-2">
-                                {
-                                    (() => {
-                                        if(RoleUser === "SuperAdmin"){
-                                            return (
-                                                <p className="">SuperAdmin</p>
-                                            )                            
-                                        }
-                                        else if(RoleUser === "Admin"){
-                                            return (                                               
-                                                <p className="">Admin</p>
-                                            )    
-                                        }
-                                        else if(RoleUser === "renter"){
-                                            return (                                                
-                                                <p className="">Renter</p>
-                                            )    
-                                        }
-                                        else if(RoleUser === "buyer"){
-                                            return (
-                                                <p className="">Buyer</p>
-                                            )    
-                                        }
-                                    })()
-                                }
-                                
-                            </div> : <Icons name="speedometer"></Icons> }
-                        </Link>
-                    </div>
-                    <hr className="mt-2 mr-4 border-b-1 border-blue-300"/>
-
-                    
-                    <div className="">
-                        {
-                            allUserSide.map((sidem) => {
-                                if(RoleUser === "SuperAdmin" || RoleUser === "SuperAdmin"){
-                                    return (
-                                        <Link to={sidem.link}>
-                                            <div className="flex py-2 text-gray-400 duration-500 hover:text-[#3B71CA]">                        
-                                                <p>{sidem.icon}</p>
-                                                <p className={`pt-2 pl-2 ${!sideOpen && 'scale-0'}`}>{sidem.name}</p>                        
-                                            </div>
-                                        </Link>
-                                    )
-                                }
-                                if(RoleUser === "renter"){
-                                    if(sidem.id === 1 || sidem.id === 2 || sidem.id === 3 || sidem.id === 4){
-                                        return (
-                                            <Link to={sidem.link}>
-                                                <div className="flex py-2 text-gray-400 duration-500 hover:text-[#3B71CA]">                        
-                                                    <p>{sidem.icon}</p>
-                                                    <p className={`pt-2 pl-2 ${!sideOpen && 'scale-0'}`}>{sidem.name}</p>                        
-                                                </div>
-                                            </Link>
-                                        )
+    if(RoleUser !== null && EmailUser !== null){
+        return (
+            <div className="bg-gray-200 py-2 h-auto">
+                <div className="flex">
+                    <div className={`duration-500 relative border-r-4 border-blue-300 shadow-xl my-1 mx-2 rounded bg-white h-auto pl-4 py-4 ${sideOpen ? "w-64" : "w-20" }`}>
+                        <div className="flex">
+                            <div className="text-[#3B71CA] pt-1" onClick={() => SetsideOpen(!sideOpen)}>{sideOpen ? <Icons size="large" name="close"></Icons> : <Icons size="large" name="menu"></Icons>}</div>
+                            {/* <div className={`text-2xl pb-4 text-[#3B71CA] font-bold ${!sideOpen && 'scale-0'}`}>SuperAdmin</div>                     */}
+                        </div>
+                        <div className="">
+                            {sideOpen ? <div>
+                                <img src="https://cdn-icons-png.flaticon.com/128/3135/3135715.png" className="px-6"/>
+                            </div> : <div></div>}
+                        </div>
+                        <div className={`pl-2 text-xl text-gray-400 duration-500 hover:text-[#3B71CA]`}>
+                            <Link to={'/Dashboard'}>
+                                {sideOpen ? <div className="flex pl-2 pt-2">
+                                    {
+                                        (() => {
+                                            if(RoleUser === "SuperAdmin"){
+                                                return (
+                                                    <p className="">SuperAdmin</p>
+                                                )                            
+                                            }
+                                            else if(RoleUser === "Admin"){
+                                                return (                                               
+                                                    <p className="">Admin</p>
+                                                )    
+                                            }
+                                            else if(RoleUser === "renter"){
+                                                return (                                                
+                                                    <p className="">Renter</p>
+                                                )    
+                                            }
+                                            else if(RoleUser === "buyer"){
+                                                return (
+                                                    <p className="">Buyer</p>
+                                                )    
+                                            }
+                                        })()
                                     }
-                                }
-                                if(RoleUser === "buyer"){
-                                    if(sidem.id === 1 || sidem.id === 2 || sidem.id === 4){
-                                        return (
-                                            <Link to={sidem.link}>
-                                                <div className="flex py-2 text-gray-400 duration-500 hover:text-[#3B71CA]">                        
-                                                    <p>{sidem.icon}</p>
-                                                    <p className={`pt-2 pl-2 ${!sideOpen && 'scale-0'}`}>{sidem.name}</p>                        
-                                                </div>
-                                            </Link>
-                                        )
-                                    }
-                                }
-                            })
-                        }
-                    </div>
-                </div>
-                <div className="w-full mr-2">
-                    <div className="py-4 rounded bg-white my-1 px-4 w-full mr-2">
+                                    
+                                </div> : <Icons name="speedometer"></Icons> }
+                            </Link>
+                        </div>
+                        <hr className="mt-2 mr-4 border-b-1 border-blue-300"/>
+    
                         
-                        <div className="flex justify-between">
-                            <p className="">VRS</p>
-                            <div className="flex ">
-                                {
-                                    navBar.map((nav) => {
-                                        if(nav.desc === "logout"){
-                                            return (
-                                                <div onClick={headlelogout} className="px-2 text-red-500 flex cursor-pointer">
-                                                    <p>{nav.name}</p>
-                                                    <p className="pl-2 pt-[2px]">{nav.icon}</p>                                               
+                        <div className="">
+                            {
+                                allUserSide.map((sidem) => {
+                                    if(RoleUser === "SuperAdmin" || RoleUser === "SuperAdmin"){
+                                        return (
+                                            <Link to={sidem.link}>
+                                                <div className="flex py-2 text-gray-400 duration-500 hover:text-[#3B71CA]">                        
+                                                    <p>{sidem.icon}</p>
+                                                    <p className={`pt-2 pl-2 ${!sideOpen && 'scale-0'}`}>{sidem.name}</p>                        
                                                 </div>
+                                            </Link>
+                                        )
+                                    }
+                                    if(RoleUser === "renter"){
+                                        if(sidem.id === 1 || sidem.id === 2 || sidem.id === 3 || sidem.id === 4){
+                                            return (
+                                                <Link to={sidem.link}>
+                                                    <div className="flex py-2 text-gray-400 duration-500 hover:text-[#3B71CA]">                        
+                                                        <p>{sidem.icon}</p>
+                                                        <p className={`pt-2 pl-2 ${!sideOpen && 'scale-0'}`}>{sidem.name}</p>                        
+                                                    </div>
+                                                </Link>
                                             )
                                         }
-                                    })
-                                }
-                            </div>
-                        </div>                      
+                                    }
+                                    if(RoleUser === "buyer"){
+                                        if(sidem.id === 1 || sidem.id === 2 || sidem.id === 4){
+                                            return (
+                                                <Link to={sidem.link}>
+                                                    <div className="flex py-2 text-gray-400 duration-500 hover:text-[#3B71CA]">                        
+                                                        <p>{sidem.icon}</p>
+                                                        <p className={`pt-2 pl-2 ${!sideOpen && 'scale-0'}`}>{sidem.name}</p>                        
+                                                    </div>
+                                                </Link>
+                                            )
+                                        }
+                                    }
+                                })
+                            }
+                        </div>
                     </div>
-                    <DashSummary />
-                </div>
-
-
+                    <div className="w-full mr-2">
+                        <div className="py-4 rounded bg-white my-1 px-4 w-full mr-2">
+                            
+                            <div className="flex justify-between">
+                                <p className="">VRS</p>
+                                <div className="flex ">
+                                    {
+                                        navBar.map((nav) => {
+                                            if(nav.desc === "logout"){
+                                                return (
+                                                    <div onClick={headlelogout} className="px-2 text-red-500 flex cursor-pointer">
+                                                        <p>{nav.name}</p>
+                                                        <p className="pl-2 pt-[2px]">{nav.icon}</p>                                               
+                                                    </div>
+                                                )
+                                            }
+                                        })
+                                    }
+                                </div>
+                            </div>                      
+                        </div>
+                        <DashSummary />
+                    </div>
+    
+    
+                        
+                    </div>
                     
-                </div>
-                
-            </div>
+                </div>    
+            
+        )
+    }
+    else{
+        localStorage.clear()
+        navigate('/')
+    }
 
-        
-    )
 }
 
 export default Dashborad
