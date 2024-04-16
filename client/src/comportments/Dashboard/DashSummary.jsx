83f28464fa52886eec65e14f82beff0bbaadc7d2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-16 12:32:19 +0530

    Update DashSummary.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+VinuraPathirana@users.noreply.github.com>
    Co-Authored-By: R.M.V.A.B Rajakaruna <160888999+infovish001@users.noreply.github.com>

--- client/src/comportments/Dashboard/DashSummary.jsx
+++ client/src/comportments/Dashboard/DashSummary.jsx
@@ -63,6 +63,51 @@ const DashSummary = () => {
                         }
                     </div>
                 </div>
+                <div className="my-8 mx-2">  
+                    <div className="lg:grid grid-cols-2 gap-4">
+                        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                    <tr>
+                                        <th scope="col" class="px-6 py-3">
+                                            Trip
+                                        </th>
+                                        <th scope="col" class="px-6 py-3">
+                                            Vehicle
+                                        </th>
+                                        <th scope="col" class="px-6 py-3">
+                                            Cost
+                                        </th>
+                                        <th scope="col" class="px-6 py-3">
+                                            View
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            Colombo
+                                        </th>
+                                        <td class="px-6 py-4">
+                                            KN-4775
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            $2999
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">View</a>
+                                            <Link>
+                                                <button className="py-2 px-1 text-blue-600 rounded duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">View</button>
+                                            </Link>
+                                        </td>
+                                    </tr>                                
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>                
+
+
+                </div>
                 <div className="bg-white rounded pr-5 shadow-xl mt-8 py-12 text-center">
                     &copy; VRS - Vehicle rental System <br />
                     Developed and Design by JehanKandy
