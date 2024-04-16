9602ab0a6ae253a25dba2ddd67c0f5fe25dfdb6d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-16 11:03:18 +0530

    Update DashSummary.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+VinuraPathirana@users.noreply.github.com>
    Co-Authored-By: R.M.V.A.B Rajakaruna <160888999+infovish001@users.noreply.github.com>

--- client/src/comportments/Dashboard/DashSummary.jsx
+++ client/src/comportments/Dashboard/DashSummary.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import Icons from "@reacticons/ionicons"
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import  secureLocalStorage  from  "react-secure-storage"
 
 const DashSummary = () => {
+    const navigate = useNavigate() 
+
+    //for open and close Side bar
+    const [sideOpen, SetsideOpen] = useState();
+    const [navOpen, SetNavOpen] = useState();
+
+    //curent login user
+    const RoleUser = secureLocalStorage.getItem("Login1");
+    const EmailUser = secureLocalStorage.getItem("login2");
+
+    
   return (
     <div>DashSummary</div>
   )
