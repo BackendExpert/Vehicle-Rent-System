6e7b2b229fdad655efbace1c7aef237ab0e91b5b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-16 11:19:37 +0530

    Update DashSummary.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+VinuraPathirana@users.noreply.github.com>
    Co-Authored-By: R.M.V.A.B Rajakaruna <160888999+infovish001@users.noreply.github.com>

--- client/src/comportments/Dashboard/DashSummary.jsx
+++ client/src/comportments/Dashboard/DashSummary.jsx
@@ -12,8 +12,35 @@ const DashSummary = () => {
 
     if(RoleUser !== null){
         return (
-            <div className="bg-white mt-4 rounded py-2 px-4">
-                asd
+            <div className="py-4">
+                {
+                    (() => {
+                        if(RoleUser === "SuperAdmin"){
+                            return (
+                                <h1 className="px-8 text-xl font-semibold">SuperAdmin Dashbord</h1>
+                            )                            
+                        }
+                        else if(RoleUser === "Admin"){
+                            return (
+                                <h1 className="px-8 text-xl font-semibold">Admin Dashbord</h1>
+                            )    
+                        }
+                        else if(RoleUser === "renter"){
+                            return (
+                                <h1 className="px-8 text-xl font-semibold">Renter Dashbord</h1>
+                            )    
+                        }
+                        else if(RoleUser === "buyer"){
+                            return (
+                                <h1 className="px-8 text-xl font-semibold">Buyer Dashbord</h1>
+                            )    
+                        }
+                    })()
+                }
+                
+                <div className="bg-white mt-4 rounded px-4">
+                    asd
+                </div>
             </div>
         )
     }
