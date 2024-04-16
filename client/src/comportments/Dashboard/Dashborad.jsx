b5c7343aed9008755b55a9e1ab9402a86085cd2b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-16 10:37:42 +0530

    Update Dashborad.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+VinuraPathirana@users.noreply.github.com>
    Co-Authored-By: R.M.V.A.B Rajakaruna <160888999+infovish001@users.noreply.github.com>

--- client/src/comportments/Dashboard/Dashborad.jsx
+++ client/src/comportments/Dashboard/Dashborad.jsx
@@ -20,14 +20,15 @@ const Dashborad = (children) => {
         {id: 1, name: "Vehicles", link: "/Vehicles", icon: <Icons name="car" size="large"></Icons>},
         {id: 2, name: "Rented Vehicle", link: "#", icon: <Icons name="car-sport" size="large"></Icons>},
         {id: 3, name: "Own Vehicle ", link: "#", icon: <Icons name="car" size="large"></Icons>},
-        {id: 4, name: "Profile", link: "#", icon: <Icons name="person" size="large"></Icons>},        
+        {id: 4, name: "Profile", link: "#", icon: <Icons name="person" size="large"></Icons>},
+        {id: 5, name: "Users", link: "#", icon: <Icons name="people" size="large"></Icons>},
+        {id: 6, name: "Requests", link: "#", icon: <Icons name="help-circle" size="large"></Icons>},
+        {id: 7, name: "Report", link: "#", icon: <Icons name="document-text" size="large"></Icons>},
+        {id: 8, name: "Suspended", link: "#", icon: <Icons name="person-circle" size="large"></Icons>},        
     ]
     //SuperAdmin Side list
     const SuperAdminSide = [
-        {name: "Users", link: "#", icon: <Icons name="people" size="large"></Icons>},
-        {name: "Requests", link: "#", icon: <Icons name="help-circle" size="large"></Icons>},
-        {name: "Report", link: "#", icon: <Icons name="document-text" size="large"></Icons>},
-        {name: "Suspended", link: "#", icon: <Icons name="person-circle" size="large"></Icons>},
+
     ]
 
     //-------Side Menus End-------------
