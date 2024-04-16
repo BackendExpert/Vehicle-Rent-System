413a2061d2e275f8241db86fc8c8d8a8b07806c0
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-16 13:49:15 +0530

    Update DashSummary.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+VinuraPathirana@users.noreply.github.com>
    Co-Authored-By: R.M.V.A.B Rajakaruna <160888999+infovish001@users.noreply.github.com>

--- client/src/comportments/Dashboard/DashSummary.jsx
+++ client/src/comportments/Dashboard/DashSummary.jsx
@@ -88,6 +88,17 @@ const DashSummary = () => {
                                         )
                                     }
                                 }
+                                if(RoleUser === "buyer"){
+                                    if(data.id === 1 || data.id === 8){
+                                        return (                                    
+                                            <div className={`cursor-pointer text-center shadow-2xl bg-white border-2 border-gray-200 rounded py-8 px-8 w-full mx-2 lg:my-0 my-2 duration-500 hover:text-sm ${data.style}`}>                                       
+                                                <p className="font-semibold text-xl">{data.icon}</p>   
+                                                <p className="font-semibold pl-2 pt-2">{data.name}</p>
+                                                <p className="font-semibold text-3xl pl-2 pt-1">{data.value}</p>
+                                            </div>                                    
+                                        )
+                                    }
+                                }
                             })
                         }
                     </div>
